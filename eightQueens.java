3966f23c51b25b256bf12542698ded0a8b377aa7
Anthea Li <antheali88@gmail.com>
2018-10-17 11:35:49 -0700

    Update

--- eightQueens.java
+++ eightQueens.java
@@ -1,10 +1,10 @@
 /**
- * @author Anthea Li
- * This program generates a window that first prompts the user for the desired number
- * of queens, then shows an accordingly-sized chess board with said amount of queens 
- * arranged to showcase the Eight Queens algorithm, where queens are placed on a chess
- * board so that none of the queens threaten each other.
- */
+* @author Anthea Li
+* This program generates a window that first prompts the user for the desired number
+* of queens, then shows an accordingly-sized chess board with said amount of queens
+* arranged to showcase the Eight Queens algorithm, where queens are placed on a chess
+* board so that none of the queens threaten each other.
+*/
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
@@ -21,8 +21,8 @@ public class eightQueens extends JPanel {
    private JLabel title;
    
    /**
-    * Create panel to display chess board
- 	*/
+   * Create panel to display chess board
+   */
    public eightQueens() {
       setLayout(new BorderLayout());
       
@@ -55,7 +55,7 @@ public class eightQueens extends JPanel {
             title.setText("Too small");
             return;
          }
-         board = new JLabel[num][num]; 
+         board = new JLabel[num][num];
          matrix = new char[num][num];
          JPanel center = new JPanel();
          center.setLayout(new GridLayout(num, num));
@@ -64,11 +64,11 @@ public class eightQueens extends JPanel {
          // add squares to board
          for(int r = 0; r < num; r++) {
             for(int c = 0; c < num; c++) {
-                board[r][c] = new JLabel();
-                board[r][c].setBorder(new LineBorder(Color.BLACK));
-                board[r][c].setIcon(null);
-                center.add(board[r][c]);
-             }
+               board[r][c] = new JLabel();
+               board[r][c].setBorder(new LineBorder(Color.BLACK));
+               board[r][c].setIcon(null);
+               center.add(board[r][c]);
+            }
          }
          run(0, 0, num);
       }
@@ -82,23 +82,23 @@ public class eightQueens extends JPanel {
             matrix = null;
             
             for(int r = 0; r < num; r++)
-               for(int c = 0; c < num; c++) 
-                  board[r][c].setIcon(null);
+            for(int c = 0; c < num; c++)
+            board[r][c].setIcon(null);
          }
       }
    }
    
-   	/**
-   	 * Checks if queen can be placed on a certain space on board
-   	 * 
-	 * @param row: row value for chess board
-	 * @param col: column value for chess board
-	 * @return true or false to indicate whether a queen can be placed
-	 * 
-	 */
-	private boolean canPlaceQueen(int row, int col) {
+   /**
+   * Checks if queen can be placed on a certain space on board
+   *
+   * @param row: row value for chess board
+   * @param col: column value for chess board
+   * @return true or false to indicate whether a queen can be placed
+   *
+   */
+   private boolean canPlaceQueen(int row, int col) {
       if(row > matrix.length || row < 0 || col > matrix[0].length || col < 0)
-         return false;
+      return false;
       for(int c1 = col - 1; c1 >= 0; c1--) {
          if(matrix[row][c1] == 'Q') {
             return false;
@@ -117,46 +117,46 @@ public class eightQueens extends JPanel {
       return true;
    }
    
-    /**
-    * Places a queen on a spot on the chess board
-	* @param row: row value for chess board
-	* @param col: column value for chess board
-	*/
-	private void placeQueen(int row, int col) {
+   /**
+   * Places a queen on a spot on the chess board
+   * @param row: row value for chess board
+   * @param col: column value for chess board
+   */
+   private void placeQueen(int row, int col) {
       matrix[row][col] = 'Q';
       board[row][col].setIcon(queenPic);
       board[row][col].setHorizontalAlignment(JLabel.CENTER);
    }
    
-	/**
-	 * Removes a queen from a spot on the chess board
-	 * 
-	 * @param row: row value for chess board
-	 * @param col: column value for chess board
-	 */
-	private void removeQueen(int row, int col) {
+   /**
+   * Removes a queen from a spot on the chess board
+   *
+   * @param row: row value for chess board
+   * @param col: column value for chess board
+   */
+   private void removeQueen(int row, int col) {
       matrix[row][col] = ' ';
       board[row][col].setIcon(null);
    }
    
    /**
-    * Runs entire operation, checks/places/removes queens accordingly
-    * 
-	* @param row: row value of chess board
-	* @param col: column value of chess board
-	* @param queens: number of queens
-	* @return true or false to stop operating when the chess board has been completed
-	*/
-	private boolean run(int row, int col, int queens) {
+   * Runs entire operation, checks/places/removes queens accordingly
+   *
+   * @param row: row value of chess board
+   * @param col: column value of chess board
+   * @param queens: number of queens
+   * @return true or false to stop operating when the chess board has been completed
+   */
+   private boolean run(int row, int col, int queens) {
       if(queens == 0)
-         return true;
+      return true;
       for(int r = row; r < num; r++) {
          if(canPlaceQueen(r, col)) {
             placeQueen(r, col);
             if(run(0, col + 1, queens - 1))
-               return true;
+            return true;
          }
-            removeQueen(r, col);
+         removeQueen(r, col);
       }
       return false;
    }
